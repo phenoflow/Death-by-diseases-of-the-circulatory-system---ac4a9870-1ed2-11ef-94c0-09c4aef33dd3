08aa70f2f9308d32710a1b9ee3aa13cc580f2b10
Martin Chapman <contact@martinchapman.co.uk>
2024-05-30 23:27:57 +0100

    Update made by Phenoflow

---
+++ python/cardiomyopathy-death-by-diseases-of-the-circulatory-system-natural-cause---secondary.py
@@ -0,0 +1,25 @@
+# John, A, McGregor, J., Jones, I., Lee, S. C., Walters, J. T. R., Owen, M. J., O'Donovan, M., DelPozo-Banos, M., Berridge, D., and Lloyd, K., 2024.
+
+import sys, csv, re
+
+codes = [{"code":"I43.2","system":"icd10"},{"code":"I42","system":"icd10"},{"code":"I42.4","system":"icd10"},{"code":"I42.6","system":"icd10"},{"code":"I43","system":"icd10"},{"code":"I43.8","system":"icd10"},{"code":"I42.8","system":"icd10"},{"code":"I42.0","system":"icd10"},{"code":"I43.1","system":"icd10"},{"code":"I42.5","system":"icd10"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('death-by-diseases-of-the-circulatory-system-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["cardiomyopathy-death-by-diseases-of-the-circulatory-system-natural-cause---secondary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["cardiomyopathy-death-by-diseases-of-the-circulatory-system-natural-cause---secondary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["cardiomyopathy-death-by-diseases-of-the-circulatory-system-natural-cause---secondary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
